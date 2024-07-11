d68a5228f7fca5304925435b4a407ba2ecc364e0
tndgoat <tungnd.goat@gmail.com>
2024-07-11 10:15:50 +0700

    init

---
+++ src/main/java/com/asm2/spring_security_radius_server/SpringSecurityRadiusServerApplication.java
@@ -0,0 +1,13 @@
+package com.asm2.spring_security_radius_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityRadiusServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityRadiusServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asm2/spring_security_radius_server/SpringSecurityRadiusServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asm2.spring_security_radius_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityRadiusServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
